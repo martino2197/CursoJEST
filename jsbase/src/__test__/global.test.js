2099b0490b9a2d56e2ee3ae3eb5b3c88143a0a99
martino2197 <martino2197@gmail.com>
2020-10-07 16:04:59 -0500

    Implementando pruebas para Boolean y Array

--- jsbase/src/__test__/global.test.js
+++ jsbase/src/__test__/global.test.js
@@ -1,5 +1,33 @@
 const text = "Hola Mundo";
 
+const fruits = ["manzana", "melon", "banana"];
+
+//strings
 test("Debe contener un texto", () => {
   expect(text).toMatch(/Mundo/);
+});
+
+//Arrays
+test("Tenemos una banana?", () => {
+  expect(fruits).toContain("banana");
+});
+
+//numbers
+test("Mayor que", () => {
+  expect(10).toBeGreaterThan(9);
+});
+
+//bool
+test("Verdadero", () => {
+  expect(true).toBeTruthy();
+});
+
+const reverseString = (str, callback) => {
+  callback(str.split("").reverse().join(""));
+};
+
+test("Probar un Callback", () => {
+  reverseString("Hola", (str) => {
+    expect(str).toBe("aloH");
+  });
 });
